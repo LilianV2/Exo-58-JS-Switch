e7629253c1a3ca01fe08cecad2cfa7c037ebc92e
lilia <lilianom59@outlook.fr>
2022-11-19 19:31:52 +0100

    exo 58

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,9 +1,15 @@
+let fruits = prompt('What fruits do u like?')
 
-(fruits) {
-    "Banana":
-    alert("Hello")
-    break;
-    "Apple":
-    alert("Welcome")
-    break;
+switch (fruits) {
+    case 'Banana':
+        alert("Hello")
+        break;
+    case 'Apple':
+        alert("Welcome")
+        break;
+    default :
+        alert("Unknown fruits :o")
 }
+
+// j'ai fait le | default | avant même avoir vu la consigne de l'exo 2
+// donc pas de 2e doss.js...
